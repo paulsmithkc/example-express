300e9e4b56590876c1e4367d0fff396b5c34de94
Paul Smith <prsmith@ranken.edu>
2020-05-12 08:33:44 -0500

    Moving example projects into their respective units

---
+++ README.md
@@ -0,0 +1,8 @@
+# Express.js Example Project
+
+To test this website:
+1. Open this folder in VSCode
+2. Open the terminal (Terminal > New Terminal)
+3. npm install
+4. npm run nodemon
+5. Open http://localhost:3000 in your browser
---
+++ index.js
@@ -0,0 +1,45 @@
+const debug = require('debug')('app:server');
+const express = require('express');
+const helmet = require('helmet');
+const path = require('path');
+
+const hostname = process.env.HOSTNAME || 'localhost';
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+// Middleware
+app.use(helmet());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, 'public/index.html'));
+});
+app.get('/contact', (request, response) => {
+  response.sendFile(path.join(__dirname, 'public/contact.html'));
+});
+app.post('/contact', (request, response) => {
+  const params = request.body;
+
+  debug('Message Received!');
+  debug(`\temail: ${params.email}`);
+  debug(`\tsubject: ${params.subject}`);
+  debug(`\tmessage: ${params.message}`);
+
+  if (params.email && params.subject && params.message) {
+    response.sendFile(path.join(__dirname, 'public/contact_sent.html'));
+  } else {
+    response.sendFile(path.join(__dirname, 'public/contact_error.html'));
+  }
+});
+
+app.use(express.static('public'));
+app.use((request, response) => {
+  response.status(404).type('text/plain').send('Page Not Found');
+});
+
+// Bind the server to an http port
+app.listen(port, hostname, () => {
+  debug(`Server running at http://${hostname}:${port}/`);
+});
